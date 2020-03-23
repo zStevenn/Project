7fb8bbac8c945234bb4c3293d1e9befaf7a6e9af
Mathijs Elbertsen <328184@student.mboutrecht.nl>
2020-03-23 12:42:43 +0100

    23-3-2020 pre-pull

--- layout-content/informatie-content/info-2.php
+++ layout-content/informatie-content/info-2.php
@@ -1,7 +1,21 @@
 <div class="row">
     <div class="col-12">
+        <h1>
+            de geschiedenis van afasie
+        </h1>
         <p>
-            
+             Volgens de meeste afasiologen begint de wetenschappelijke afasiestudie in de tweede helft van de 19e
+            eeuw, toen Broca en Wernicke een beschrijving gaven van de twee klassieke afasietypen die nu hun naam dragen (lees hier meer over bij soorten).
+             Hoewel de interesse voor en het inzicht in afasie hierna sterk toenamen, is het onjuist om alle
+            eerdere beschrijvingen van afasie als onbelangrijk, bizar of “prehistorisch” terzijde te schuiven.
+             Ditbartikel geeft een overzicht van de vroege geschiedenis van de afasiologie, van de allereerste observatie van “sprakeloosheid” in een
+            Egyptische papyrus (ca. 1700 v. Chr.) tot aan Broca’s baanbrekende artikelen in 1861. 
+            Hoewel veel symptomen en vormen van afasie al voor 1800 beschreven waren, werden significante hypotheses over de lokalisatie van afasie pas in de
+            periode 1800-1860 geformuleerd.
+             Op basis van zijn pseudo-wetenschappelijke frenologische theorie (of “Schädellehre”) lokaliseerde Gall als eerste taal in de
+            frontale cortex (Gall & Stuart, 1806). 
+             Gall’s hypothese dat het “spraakcentrum” in het voorste deel van de hersenen was gesitueerd, werd later gesteund door de
+            neuropathologische data van Bouillaud (1825), die daarmee de grondslag legde voor Broca’s befaamde ontdekking van het “spraakcentrum” in 1861
         </p>
     </div>
 </div>
---
+++ layout-content/informatie-content/info-5.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col-12">
+        <h1>
+            Behandeling afasie
+        </h1>
+        <p>
+            Logopedie bevordert het herstel, maar volledig herstellen is meestal niet mogelijk. Wel kan een
+            afasiepatiënt beter leren omgaan met de beperkingen.
+        </p>
+        <p>
+            Dankzij haar donateurs kon de Hersenstichting bijdragen aan de ontwikkeling van Kompro 2000. Dit is een
+            computer-oefenprogramma dat de Afasie Vereniging Nederland heeft ontwikkeld. Het bestaat uit duizenden
+            afbeeldingen, waarbij men zinsbouw-, geheugen- en uitspraakoefeningen op verschillende niveaus kan doen. Het
+            stelt de persoon met afasie onder andere in staat te oefenen zonder de partner steeds te moeten belasten.
+        </p>
+    </div>
+</div>
